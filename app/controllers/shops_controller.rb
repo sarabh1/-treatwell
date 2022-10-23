7e90b096788cada6f831e09bd258afbf84c03ab5
OpheNg <ophe.ng@live.fr>
2022-10-23 19:52:07 +0200

    galère absolue, les create ne marchent pas dans le terminal COINCEE

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,2 +1,10 @@
 class ShopsController < ApplicationController
+  # def new
+  #   @shop = Shop.new
+  #   @user= current_user
+  # end
+  # def create
+  #   @shop = Shop.new
+  #   @user= current_user
+  # end
 end
